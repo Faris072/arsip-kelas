3edd92b0881a6a57cd44613a0fbe5bda6a0a0d53
M. Faris shafwan <farisbos.mfs@gmail.com>
2022-03-12 12:20:34 +0700

    up

---
+++ app/Http/Controllers/tugasController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tugas;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class tugasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tugas  $tugas
+     * @return \Illuminate\Http\Response
+     */
+    public function show($tugas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tugas  $tugas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($tugas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tugas  $tugas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request,$tugas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tugas  $tugas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($tugas)
+    {
+        //
+    }
+}
